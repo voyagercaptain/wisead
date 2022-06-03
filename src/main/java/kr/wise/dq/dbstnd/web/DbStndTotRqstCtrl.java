ac92a9fdd3b41cdc29f3bb362ec4935e1bd23596
admin <admin@host.docker.internal>
2022-06-03 16:18:36 +0900

    기관목록 전체조회 행안부 권한 변경

--- src/main/java/kr/wise/dq/dbstnd/web/DbStndTotRqstCtrl.java
+++ src/main/java/kr/wise/dq/dbstnd/web/DbStndTotRqstCtrl.java
@@ -192,7 +192,7 @@ public class DbStndTotRqstCtrl {
         
         List<CodeListVo> userOrgList = null;
         
-        if ("2".equals(usergId) || "OBJ_00000186517".equals(usergId)) {
+        if ("2".equals(usergId) || "3".equals(usergId)) {
 			 userOrgList = codeListService.getCodeList(WiseMetaConfig.CodeListAction.orgCd); 
 		 } 
 		 else {
