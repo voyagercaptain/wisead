bedf16ab21ce5262f0e4a23272c050c35860c003
admin <admin@host.docker.internal>
2022-06-13 14:12:02 +0900

    IBSheet 라이센스 업데이트

--- src/main/webapp/js/IBSheet/ibleaders.js
+++ src/main/webapp/js/IBSheet/ibleaders.js
@@ -24,6 +24,6 @@ ibleaders = {
      * licenseType이 "file" 일 경우 라이선스 파일 명을 기입한다.
      */
     // license: "license",
-    license: "W2FtSztPKC9zbDcwYjJxbn8QakAsTjVoJD94ZDRnLH4oMF9gCyhXL3ZsMSpjMXojZnYhSz9dKVU/eD8tLmQ6dikhYndSOF0sWW4pN201ej98cWRpOkw/UmgWZmAwYzd3Ig=="
-
+    //license: "W2FtSztPKC9zbDcwYjJxbn8QakAsTjVoJD94ZDRnLH4oMF9gCyhXL3ZsMSpjMXojZnYhSz9dKVU/eD8tLmQ6dikhYndSOF0sWW4pN201ej98cWRpOkw/UmgWZmAwYzd3Ig=="
+	license: "W2FtSztPKCNybzcwYjJxbn9SMVxtGyknYmkyMGk6ZH49IAF8H3tSJ3JyNjdgLWEmYHYhSCJdPEVhZCt+K24wcj8uYHJSPUQlQW82Kns+f2QuJz8wclc8UiULYWIxfzV2PyNqaHArBDRLZB8yfjBjJmlvMDNpMXNFNBF/CGspK3IlI2RmNmw1Z1t/EDZWKW48NjV6LHo4cyB+FWlDLEkwdzg4Z3spbyklemYEMUYpSCZ3cjYzYC1kIH53K1N5GnpAPzZuYC9hMXc/LmRyUjhCO0BlLXE8eHogdmk+M3RXO1Y6FWZnL34/bWRkLG0ueUgrU3AcNH43Zzl3aDUtaDtoHnNXekw7ZTx4Jjl/ZSl6NmFEYxk2ViluPDY1ejZmMT8qYBZqQDBMNQ=="
 };
